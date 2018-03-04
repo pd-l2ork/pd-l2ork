8c2eba77b460f62e3d7931c7c79184a3e15c50e4
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2018-03-04 15:22:56 -0500

    git rid of old flext stuff and use submodules for new flext and stk

--- l2ork_addons/tar_em_up.sh
+++ l2ork_addons/tar_em_up.sh
@@ -249,11 +249,9 @@ then
 	then
 		echo "installing desktop version..."
 		cp -f debian/control.desktop debian/control
-		cp -f ../../l2ork_addons/flext/config-lnx-pd-gcc.txt.intel ../../externals/grill/trunk/flext/buildsys/config-lnx-pd-gcc.txt
 	else
 		echo "installing raspbian version..."
 		cp -f debian/control.raspbian debian/control
-		cp -f ../../l2ork_addons/flext/config-lnx-pd-gcc.txt.rpi ../../externals/grill/trunk/flext/buildsys/config-lnx-pd-gcc.txt
 		cat ../../externals/OSCx/src/Makefile | sed -e s/-lpd//g > ../../externals/OSCx/src/Makefile
 	fi
 	if [[ $os == "win" ]]; then
