1ae93f5bd4ef70ce569c08217c786f53e5d032c0
U-MSI\Ico Bukvic <ico@vt.edu>
2024-10-13 00:28:03 -0400

    *Added interactive option to the L2OrkTweet dropdowns, version bump.

--- pd/src/m_pd.h
+++ pd/src/m_pd.h
@@ -11,10 +11,10 @@ extern "C" {
 #include <stdarg.h>
 
 #define PD_MAJOR_VERSION 0
-#define PD_MINOR_VERSION 54
+#define PD_MINOR_VERSION 55
 #define PD_BUGFIX_VERSION 0
 #define PD_TEST_VERSION ""
-#define PD_L2ORK_VERSION "20240930"
+#define PD_L2ORK_VERSION "20241014"
 #define PDL2ORK
 extern int pd_compatibilitylevel;   /* e.g., 43 for pd 0.43 compatibility */
 
