f2163bad153f17e007607e54d8c291eb7f557f11
U-MSI\Ico Bukvic <ico@vt.edu>
2024-10-15 15:43:30 -0400

    *Fixed wrong help file referencing for expr~ and vexpr~

--- pd/src/x_vexp_if.c
+++ pd/src/x_vexp_if.c
@@ -793,6 +793,7 @@ expr_setup(void)
         class_addfloat(exprproxy_class, exprproxy_float);
         class_addmethod(expr_class,(t_method)expr_version,
                                                         gensym("version"), 0);
+        class_sethelpsymbol(expr_class, gensym("expr"));
 
         /*
          * expr~ initialization
@@ -802,7 +803,7 @@ expr_setup(void)
         class_addmethod(expr_tilde_class, nullfn, gensym("signal"), 0);
         CLASS_MAINSIGNALIN(expr_tilde_class, t_expr, exp_f);
         class_addmethod(expr_tilde_class,(t_method)expr_dsp, gensym("dsp"), 0);
-        class_sethelpsymbol(expr_tilde_class, gensym("expr"));
+        class_sethelpsymbol(expr_tilde_class, gensym("expr~"));
         class_addmethod(expr_tilde_class,(t_method)expr_version,
                                                         gensym("version"), 0);
         /*
@@ -825,7 +826,7 @@ expr_setup(void)
                                                         gensym("verbose"), 0);
         class_addmethod(fexpr_tilde_class,(t_method)expr_version,
                                                         gensym("version"), 0);
-        class_sethelpsymbol(fexpr_tilde_class, gensym("expr"));
+        class_sethelpsymbol(fexpr_tilde_class, gensym("fexpr~"));
 
 }
 
