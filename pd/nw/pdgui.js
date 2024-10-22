714f18f1e36f9ad1234e504be26380dc6fb678e7
U-MSI\Ico Bukvic <ico@vt.edu>
2024-10-22 15:31:21 -0400

    *Added refresh option from Purr-Data for audio and MIDI devices. Updated TODO

--- pd/nw/pdgui.js
+++ pd/nw/pdgui.js
@@ -9341,6 +9341,7 @@ function gui_recent_files(dummy, recent_files_array) {
 
 function gui_audio_properties(gfxstub, sys_indevs, sys_outdevs,
     pd_indevs, pd_inchans, pd_outdevs, pd_outchans, audio_attrs) {
+    post("gui_audio_properties " + sys_outdevs);
     var attrs = audio_attrs.concat([
         "audio-apis", pd_apilist,
         "sys-indevs", sys_indevs,
