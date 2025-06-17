b59ac8172e1076cd7edade9589a1f7b2a5489829
Movement <48330137+MovementGH@users.noreply.github.com>
2025-06-17 16:49:50 -0400

    Fix file dialogs

--- pd/src/x_gui.c
+++ pd/src/x_gui.c
@@ -263,6 +263,8 @@ typedef struct _filedialog
     t_symbol *x_s;
 } t_filedialog;
 
+static t_filedialog *filedialog_obj;
+
 static t_filedialog *filedialog_new( void)
 {
     char buf[50];
@@ -270,7 +272,7 @@ static t_filedialog *filedialog_new( void)
     sprintf(buf, "d%zx", (t_uint)x);
     x->x_s = gensym(buf);
     pd_bind(&x->x_obj.ob_pd, x->x_s);
-    gui_vmess("file_dialog_obj", "s", x->x_s->s_name);
+    filedialog_obj = x;
     return (x);
 }
 
@@ -408,6 +410,7 @@ static void *openpanel_new(t_floatarg mode)
     x->x_s = gensym(buf);
     pd_bind(&x->x_obj.ob_pd, x->x_s);
     outlet_new(&x->x_obj, &s_symbol);
+    gui_vmess("file_dialog_obj", "s", filedialog_obj->x_s->s_name);
     return (x);
 }
 
@@ -471,6 +474,7 @@ static void *savepanel_new( void)
     x->x_canvas = canvas_getcurrent();
     pd_bind(&x->x_obj.ob_pd, x->x_s);
     outlet_new(&x->x_obj, &s_symbol);
+    gui_vmess("file_dialog_obj", "s", x->x_s->s_name);
     return (x);
 }
 
